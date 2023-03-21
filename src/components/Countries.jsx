f945780acdc486cf38acd35ed12d08a89eacdab5
Andrea <andrearivas4cinfor@gmail.com>
2023-03-21 09:32:41 +0100

    display country details

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import styled from 'styled-components'
 import { fetchCountries, selectCountries } from '../redux/countriesSlice';
 
 const Paintings = () => {
@@ -13,16 +14,43 @@ const Paintings = () => {
   }), [dispatch, countries];
  console.log(countries)
   return (
-  <div>
+  <Wrapper >
     {countries.map((country)=> {
     return (
-      <div>
-        <h2>Country</h2>
-    
+      <div className="country-card" key={country.cca3}>
+        <img
+          src={country.flags.png}
+          alt={country.flag.alt}
+          className="flag"
+        ></img>
+        <h2>{country.name.common}</h2>
+        <p>
+          <span>Capital:</span> {country.capital}
+        </p>
+        <p>
+          <span>Population:</span> {country.population}
+        </p>
+        <p>
+          <span>Region:</span> {country.region}
+        </p>
       </div>
     )
   })}
-  </div>
+  </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+display:grid;
+grid-template-columns: repeat(2, 1fr);
+.country-card {
+  border: 1px solid;
+  .flag {
+    width: 200px;
+    height: auto;
+  }
+}
+
+`
+
 export default Paintings;
