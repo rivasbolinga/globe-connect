1abd00f5d6984b8b8d76119d3b2dc82f1946a7a8
Andrea <andrearivas4cinfor@gmail.com>
2023-03-22 20:05:39 +0100

    Style home page

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -3,33 +3,27 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 // -- Countries component receiving props from dispatch fetchCountries in HomePage
-const Countries = ({
-  num, flag, name, capital, population, region,
-}) => (
+const Countries = ({ num, flag, name, capital, population, region }) => (
   <Wrapper className="country-card">
     <Link to={`/country/${num}`} className="link">
       <img src={flag.png} alt={flag.alt} className="flag" />
-      <h2>{name}</h2>
-      <p>
-        <span>Capital:</span>
-        {' '}
-        {/* For the cases where there is not capital */}
-        {capital && capital.length > 0 ? capital : 'Has no capital'}
-      </p>
-      <p>
-        {/* cases with not population */}
-        <span>Population:</span>
-        {' '}
-        {population || 'Ups! nobody leaves here'}
-      </p>
-      <p>
-        <span>Region:</span>
-        {' '}
-        {region}
-      </p>
+      <div className="text">
+        <h2>{name}</h2>
+        <p>
+          <span>Capital:</span> {/* For the cases where there is not capital */}
+          {capital && capital.length > 0 ? capital : 'Has no capital'}
+        </p>
+        <p>
+          {/* cases with not population */}
+          <span>Population:</span> {population || 'Ups! nobody leaves here'}
+        </p>
+        <p>
+          <span>Region:</span> {region}
+        </p>
+      </div>
     </Link>
   </Wrapper>
-);
+)
 // use of oneOfType because capital can come as sting or array (or not  capital at all).
 Countries.propTypes = {
   num: PropTypes.string.isRequired,
@@ -45,13 +39,30 @@ Countries.defaultProps = {
 };
 
 const Wrapper = styled.section`
-  border: 1px solid;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  padding-bottom: 20px;
   .flag {
-    width: 200px;
+    width: 100%;
     height: auto;
+  }
+  .text {
+    display: flex;
+    flex-direction: column;
+    gap:4px
+  }
+  h2 {
+    font-size: 20px;
+    color: #363b9d;
+    margin: 8px auto 5px auto;
+  }
+  p {
+    font-size: 12px;
   }
 }
 
-`;
+`
 
 export default Countries;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { fetchCountries, selectCountries } from '../redux/countriesSlice';
 import Countries from '../components/Countries';
 import { openModal, selectSelectedContinent } from '../redux/modalSlice';
 
+
 const Home = () => {
   const dispatch = useDispatch();
   const { countries } = useSelector(selectCountries);
@@ -20,51 +21,69 @@ const Home = () => {
   // -- Create a variable with all the countries that has the selected.
   const filteredCountries = countries.filter((ct) => ct.continents.includes(selectedContinent));
   return (
-    <Wrapper>
-      <div>
+    <Wrapper className='home'>
+      <div className='header'>
+        <img src="https://img.icons8.com/dusk/64/null/geography--v1.png" />
         <h1>Countries</h1>
         <button type="button" onClick={() => dispatch(openModal())}>
           {' '}
-          settings
+          <i className="fa-solid fa-gear"></i>
         </button>
       </div>
-      {filteredCountries.length
-        ? filteredCountries.map((country) => (
-          <Countries
-            key={country.cca3}
-            num={country.cca3}
-            flag={country.flags}
-            name={country.name.common}
-            capital={country.capital}
-            population={country.population}
-            region={country.region}
-          />
-        ))
-        : countries.map((country) => (
-          <Countries
-            key={country.cca3}
-            num={country.cca3}
-            flag={country.flags}
-            name={country.name.common}
-            capital={country.capital}
-            population={country.population}
-            region={country.region}
-          />
-        ))}
+      <div className='country-list'>
+        {filteredCountries.length
+          ? filteredCountries.map((country) => (
+              <Countries
+                key={country.cca3}
+                num={country.cca3}
+                flag={country.flags}
+                name={country.name.common}
+                capital={country.capital}
+                population={country.population}
+                region={country.region}
+              />
+            ))
+          : countries.map((country) => (
+              <Countries
+                key={country.cca3}
+                num={country.cca3}
+                flag={country.flags}
+                name={country.name.common}
+                capital={country.capital}
+                population={country.population}
+                region={country.region}
+              />
+            ))}
+      </div>
     </Wrapper>
-  );
+  )
 };
 
 const Wrapper = styled.section`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  .country-card {
-    border: 1px solid;
-    .flag {
-      width: 200px;
-      height: auto;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background-color: white;
+  .header {
+    display: flex;
+    gap: 15px;
+    padding: 10px;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 0.5px solid grey;
+    img {
+      width: 60px;
+      height: 60px;
     }
   }
-`;
+  .country-list {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 0;
+  }
+@media (min-width: 425px) {
+  width: 425px;
+}
+`
 
 export default Home;
